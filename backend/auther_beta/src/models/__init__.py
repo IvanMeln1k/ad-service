e17f67a0fabd31b9f774acf72f524b39cb74e844
IvanMeln1k <IvanMeln1k@users.noreply.github.com>
2026-04-14 18:38:40 +0300

    Remove legacy auther_beta, clean up auther config

--- backend/auther_beta/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.models.categories_tasks import CategoriesTasks
-from src.models.categories import Categories
-from src.models.tasks import Tasks
-from src.models.users import Users
-from src.models.sessions import Sessions
--- backend/auther_beta/src/routers/routers.py
+++
@@ -1,7 +0,0 @@
-from src.routers.auth import auth_router
-from src.routers.users import users_router
-from src.routers.categories import categories_router
-
-
-all_routers = [auth_router, users_router, categories_router]
-
